e8f4c61df1b8f48dc0c2a5ae4bdad6422bb1aa00
yong seock <myongdol132@gmail.com>
2023-09-26 22:07:45 +0900

    View, Text 컴포넌트 학습

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!!!!</Text>
-      <StatusBar style="auto" />
+      <Text>aa</Text>
+        <View>
+        <Text>dd</Text>
+        <Text>dd</Text>
+        </View>
+      <Text>aa</Text>
+      <Button title="클릭" />
     </View>
   );
 }
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# Learn_ReactNative" 
+[View, Text 컴포넌트](https://blog.naver.com/ares132/223222490970)
