fcf4eed04a0619821f9cac1dcb098cfa15d7c69e
yong seock <myongdol132@gmail.com>
2023-09-28 23:58:35 +0900

    레이아웃 기초작업과 플렉스 박스 사용하기

--- App.js
+++ App.js
@@ -1,30 +1,32 @@
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>aa</Text>
-        <View>
-        <Text>dd</Text>
-        <Text>dd</Text>
-        </View>
-      <Text style={styles.myText}>안녕하세요</Text>
-      <Button title="클릭" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='목표를 입력 해주세요.'/>
+        <Button title='목표추가'/>
+      </View>
+      <View>
+        <Text>목표 리스트</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+   padding: 50 
   },
-  myText: {
-    margin: 16,
-    borderWidth: 2,
-    borderColor: 'red',
-    padding: 16
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '80%',
+    marginRight: 8,
+    padding: 8,
   }
 });
