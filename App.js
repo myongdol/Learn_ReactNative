fc675e3235d52e19c45919b7a0ec0ab0c08eec7d
yong seock <myongdol132@gmail.com>
2023-09-29 03:06:41 +0900

    레이아웃 기본 작업

--- App.js
+++ App.js
@@ -1,79 +1,42 @@
-// import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
-
-// export default function App() {
-//   return (
-//     <View style={styles.appContainer}>
-//       <View style={styles.inputContainer}>
-//         <TextInput style={styles.textInput} placeholder='목표를 입력 해주세요.'/>
-//         <Button title='목표추가'/>
-//       </View>
-//       <View>
-//         <Text>목표 리스트</Text>
-//       </View>
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   appContainer: {
-//    padding: 50 
-//   },
-//   inputContainer: {
-//     flexDirection: 'row',
-//     justifyContent: 'space-between'
-//   },
-//   textInput: {
-//     borderWidth: 1,
-//     borderColor: '#cccccc',
-//     width: '80%',
-//     marginRight: 8,
-//     padding: 8,
-//   }
-// });
-import React from 'react';
-import { Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{
-      padding: 50,
-      flexDirection: 'row',
-      width: '80%',
-      height: 300,
-      justifyContent: 'space-around',
-      alignItems: 'stretch',
-      }}
-    >
-      <View
-        style={{
-          backgroundColor: 'red',
-          justifyContent: 'center',
-          alignItems: 'center',
-          flex: 1,
-        }}
-      >
-        <Text>1</Text>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='목표를 입력 해주세요.'/>
+        <Button title='목표 추가'/>
       </View>
-      <View
-        style={{
-          backgroundColor: 'blue',
-          justifyContent: 'center',
-          alignItems: 'center',
-          flex: 1,
-        }}
-      >
-        <Text>2</Text>
-      </View>
-      <View
-        style={{
-          backgroundColor: 'green',
-          justifyContent: 'center',
-          alignItems: 'center',
-          flex: 1,
-        }}
-      >
-        <Text>3</Text>
+      <View style={styles.listContainer}>
+        <Text>목표 리스트</Text>
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  appContainer: {
+   flex: 1,
+   padding: 50,
+   paddingHorizontal: 16
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc'
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '70%',
+    marginRight: 8,
+    padding: 8,
+  },
+  listContainer: {
+    flex: 6
+  }
+});
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 [View, Text 컴포넌트](https://blog.naver.com/ares132/223222490970)  
 [컴포넌트 스타일링 학습](https://blog.naver.com/ares132/223222544872)  
 [플렉스 박스 알아보기](https://blog.naver.com/ares132/223224199431)  
+[레이아웃 기본 작업](https://blog.naver.com/ares132/223224205042)  
